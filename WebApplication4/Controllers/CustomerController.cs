74e822859f1171b6594540348ece79bf13c19fbf
Josh Cornwell <joshiecorn@gmail.com>
2021-11-02 18:10:40 +0000

    unzipped.

---
+++ WebApplication4/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public CustomerController()
+        {
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> postCustomer(Customer customer)
+        {
+            return BadRequest();
+        }
+
+    }
+}
